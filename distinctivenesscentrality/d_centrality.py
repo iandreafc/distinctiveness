95fc4f3e17bb7022ddd304e49dff4cada4ab61b9
Andrea Fronzetti Colladon <52541209+andreafcdev@users.noreply.github.com>
2019-12-10 12:50:18 +0100

    Update d_centrality.py

--- distinctivenesscentrality/d_centrality.py
+++ distinctivenesscentrality/d_centrality.py
@@ -2,13 +2,45 @@ import networkx as nx
 import numpy as np
 
 
-def d_preprocess(G):
+def d_preprocess(G, alpha = 1):
     
     #Make an independent copy of the graph
     G = G.copy()
     
-    #Check Graph Type
-    #Currently only undirected graphs are accepted
+    #Check Graph Type and Convert MultiGraph to Graph and directed to undirected
+    if type(G) == nx.MultiGraph:
+        G1 = nx.Graph()
+        for u,v,data in G.edges(data=True):
+            w = data['weight'] if 'weight' in data else 1.0
+            if G1.has_edge(u,v):
+                G1[u][v]['weight'] += w
+            else:
+                G1.add_edge(u, v, weight=w)
+        G = G1.copy()
+    elif type(G) == nx.MultiDiGraph:
+        #Convert to DiGraph
+        G1 = nx.DiGraph()
+        for u,v,data in G.edges(data=True):
+            w = data['weight'] if 'weight' in data else 1.0
+            if G1.has_edge(u,v):
+                G1[u][v]['weight'] += w
+            else:
+                G1.add_edge(u, v, weight=w)
+        
+        #Make undirected
+        UG = G1.to_undirected()
+        for node in G1:
+            for ngbr in nx.neighbors(G1, node):
+                if node in nx.neighbors(G1, ngbr):
+                    UG.edges[node, ngbr]['weight'] = (G1.edges[node, ngbr]['weight'] + G1.edges[ngbr, node]['weight'])
+        G = UG.copy()
+    elif type(G) == nx.DiGraph:
+        UG = G.to_undirected()
+        for node in G:
+            for ngbr in nx.neighbors(G, node):
+                if node in nx.neighbors(G, ngbr):
+                    UG.edges[node, ngbr]['weight'] = (G.edges[node, ngbr]['weight'] + G.edges[ngbr, node]['weight'])
+        G = UG.copy()
     
     #Remove Loops
     if list(G.selfloop_edges()):
@@ -30,30 +62,38 @@ def d_preprocess(G):
     
     #Calculates degree and weighted degree
     deg = dict(nx.degree(G))
-    wei_deg = dict(nx.degree(G, weight ="weight"))
     
+    if alpha > 1:
+        wei_deg = dict(nx.degree(G, weight ="weight"))
+        wei_deg_alpha = {k:v**alpha for k,v in wei_deg.items()}
+    else:
+        wei_deg_alpha = dict(nx.degree(G, weight ="weight"))
     
-    return G, n1, deg, wei_deg, totalWEI
+    return G, n1, deg, wei_deg_alpha, totalWEI
 
 
 #G is a 
-def d_all (G, normalize = False):
+def d_all (G, alpha = 1, normalize = False):
     
-    G, n1, deg, wei_deg, totalWEI = d_preprocess(G)
+    if alpha < 1:
+        print("WARNING. Alpha cannot be lower than 1. The value is set to 1.")
+        alpha = 1
+    
+    G, n1, deg, wei_deg_alpha, totalWEI = d_preprocess(G, alpha = alpha)
     
     #Computes Distinctiveness Centrality, all 5 metrics
     distinctiveness = {}
     for u,v,data in G.edges(data=True):    
-        d1u = data['weight'] * np.log10(n1/deg[v])
-        d1v = data['weight'] * np.log10(n1/deg[u])
-        d2u = 1 * np.log10(n1/deg[v])
-        d2v = 1 * np.log10(n1/deg[u])
-        d3u = data['weight'] * np.log10(totalWEI/(wei_deg[v] - data['weight'] + 1))
-        d3v = data['weight'] * np.log10(totalWEI/(wei_deg[u] - data['weight'] + 1))
-        d4u = data['weight'] * (data['weight'] / wei_deg[v])
-        d4v = data['weight'] * (data['weight'] / wei_deg[u])
-        d5u = 1* (1 / deg[v])
-        d5v = 1* (1 / deg[u])
+        d1u = data['weight'] * np.log10(n1/(deg[v])**alpha)
+        d1v = data['weight'] * np.log10(n1/(deg[u])**alpha)
+        d2u = 1 * np.log10(n1/(deg[v])**alpha)
+        d2v = 1 * np.log10(n1/(deg[u])**alpha)
+        d3u = data['weight'] * np.log10(totalWEI/(wei_deg_alpha[v] - data['weight'] + 1))
+        d3v = data['weight'] * np.log10(totalWEI/(wei_deg_alpha[u] - data['weight'] + 1))
+        d4u = data['weight'] * (data['weight'] / wei_deg_alpha[v])
+        d4v = data['weight'] * (data['weight'] / wei_deg_alpha[u])
+        d5u = 1* (1 / (deg[v])**alpha)
+        d5v = 1* (1 / (deg[u])**alpha)
         try:
             distinctiveness[u] = [x + y for x, y in zip(distinctiveness[u], [d1u,d2u,d3u,d4u,d5u])]
         except:
