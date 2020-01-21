dddc67fcbd45ef93f06a4be52c3ae692397fd7d4
Andrea Fronzetti Colladon <52541209+andreafcdev@users.noreply.github.com>
2020-01-21 14:12:38 +0100

    Update dc.py

--- distinctiveness/dc.py
+++ distinctiveness/dc.py
@@ -56,29 +56,36 @@ def g_preprocess(G, alpha = 1):
     #Calculates degree and weighted degree
     if type(G) == nx.Graph:
         deg = dict(nx.degree(G))
-        indeg = outdeg = wei_indeg_alpha = wei_outdeg_alpha = np.nan
+        indeg = outdeg = wei_insum_alpha = wei_outsum_alpha = np.nan
         
         #Calculate weighted degree, taking alpha into account
-        wei_deg_alpha = dict(nx.degree(G, weight ="weight"))
-        if alpha > 1:
-            wei_deg_alpha = {k:v**alpha for k,v in wei_deg_alpha.items()}
+        if alpha != 1:
+            wei_sum_alpha = {}
+            for node in G.nodes():
+                wei_sum_alpha[node] = sum([e[2]["weight"]**alpha for e in list(G.edges(node, data = True))])
+        else:
+           wei_sum_alpha = dict(nx.degree(G, weight ="weight")) 
             
     elif type(G) == nx.DiGraph:
-        deg = wei_deg_alpha = np.nan
+        deg = wei_sum_alpha = np.nan
         indeg = dict(G.in_degree())
         outdeg = dict(G.out_degree())
         
-        wei_indeg_alpha = dict(G.in_degree(weight ="weight"))
-        wei_outdeg_alpha = dict(G.out_degree(weight ="weight"))
-        if alpha > 1:
-            wei_indeg_alpha = {k:v**alpha for k,v in wei_indeg_alpha.items()}
-            wei_outdeg_alpha = {k:v**alpha for k,v in wei_outdeg_alpha.items()}
+        if alpha != 1:
+            wei_outsum_alpha = {}
+            wei_insum_alpha = {}
+            for node in G.nodes():
+                wei_outsum_alpha[node] = sum([e[2]["weight"]**alpha for e in list(G.out_edges(node, data = True))])
+                wei_insum_alpha[node] =  sum([e[2]["weight"]**alpha for e in list(G.in_edges(node, data = True))])
+        else:
+            wei_insum_alpha = dict(G.in_degree(weight ="weight"))
+            wei_outsum_alpha = dict(G.out_degree(weight ="weight"))
             
         
     #Calculates max arc weight
     maxwij = max(dict(G.edges).items(), key=lambda x: x[1]['weight'])[1]["weight"]
     
-    return G, n1, deg, indeg, outdeg, wei_indeg_alpha, wei_outdeg_alpha, wei_deg_alpha, totalWEI, maxwij
+    return G, n1, deg, indeg, outdeg, wei_insum_alpha, wei_outsum_alpha, wei_sum_alpha, totalWEI, maxwij
 
 
 
@@ -88,7 +95,7 @@ def dc_all (G, alpha = 1, normalize = False):
     if alpha < 1:
         print("WARNING. Alpha should be >= 1, except you exactly know what you are doing.")
     
-    G, n1, deg, indeg, outdeg, wei_indeg_alpha, wei_outdeg_alpha, wei_deg_alpha, totalWEI, maxwij = g_preprocess(G, alpha = alpha)
+    G, n1, deg, indeg, outdeg, wei_insum_alpha, wei_outsum_alpha, wei_sum_alpha, totalWEI, maxwij = g_preprocess(G, alpha = alpha)
     Glist = list(G.nodes)
 
     #Define max of all metrics
@@ -100,7 +107,10 @@ def dc_all (G, alpha = 1, normalize = False):
         D5max = n1
         D1min = (1-alpha) * maxwij * np.log10(n1)
         D2min = (1-alpha) * np.log10(n1)
-        D3min = 
+        if alpha > 1:
+            D3min = maxwij * np.log10(maxwij * n1 / ((n1-1) * (maxwij**alpha) +1))
+        else:
+            D3min = 0 #isolates
         D4min = 0
         D5min = 0 #considers isolates
     else:
@@ -120,11 +130,11 @@ def dc_all (G, alpha = 1, normalize = False):
             d2[u] += ((1 * np.log10(n1 / deg[v]**alpha)) - D2min) / (D2max - D2min)
             d2[v] += ((1 * np.log10(n1 / deg[u]**alpha)) - D2min) / (D2max - D2min)
             
-            d3[u] += ((data['weight'] * np.log10(totalWEI/(wei_deg_alpha[v] - data['weight']**alpha + 1))) - D3min) / (D3max - D3min)
-            d3[v] += ((data['weight'] * np.log10(totalWEI/(wei_deg_alpha[u] - data['weight']**alpha + 1))) - D3min) / (D3max - D3min)
+            d3[u] += ((data['weight'] * np.log10(totalWEI/(wei_sum_alpha[v] - data['weight']**alpha + 1))) - D3min) / (D3max - D3min)
+            d3[v] += ((data['weight'] * np.log10(totalWEI/(wei_sum_alpha[u] - data['weight']**alpha + 1))) - D3min) / (D3max - D3min)
             
-            d4[u] += ((data['weight'] * (data['weight']**alpha / wei_deg_alpha[v])) - D4min) / (D4max - D4min)
-            d4[v] += ((data['weight'] * (data['weight']**alpha / wei_deg_alpha[u])) - D4min) / (D4max - D4min)
+            d4[u] += ((data['weight'] * (data['weight']**alpha / wei_sum_alpha[v])) - D4min) / (D4max - D4min)
+            d4[v] += ((data['weight'] * (data['weight']**alpha / wei_sum_alpha[u])) - D4min) / (D4max - D4min)
             
             d5[u] += ((1* (1 / deg[v]**alpha)) - D5min) / (D5max - D5min)
             d5[v] += ((1* (1 / deg[u]**alpha)) - D5min) / (D5max - D5min)
@@ -141,11 +151,11 @@ def dc_all (G, alpha = 1, normalize = False):
             d2_in[v] += ((1 * np.log10(n1 / outdeg[u]**alpha)) - D2min) / (D2max - D2min)
             d2_out[u] += ((1 * np.log10(n1 / indeg[v]**alpha)) - D2min) / (D2max - D2min)
             
-            d3_in[v] += ((data['weight'] * np.log10(totalWEI/(wei_outdeg_alpha[u] - data['weight']**alpha + 1))) - D3min) / (D3max - D3min)
-            d3_out[u] += ((data['weight'] * np.log10(totalWEI/(wei_indeg_alpha[v] - data['weight']**alpha + 1))) - D3min) / (D3max - D3min)
+            d3_in[v] += ((data['weight'] * np.log10(totalWEI/(wei_outsum_alpha[u] - data['weight']**alpha + 1))) - D3min) / (D3max - D3min)
+            d3_out[u] += ((data['weight'] * np.log10(totalWEI/(wei_insum_alpha[v] - data['weight']**alpha + 1))) - D3min) / (D3max - D3min)
             
-            d4_in[v] += ((data['weight'] * (data['weight']**alpha / wei_outdeg_alpha[u])) - D4min) / (D4max - D4min)
-            d4_out[u] += ((data['weight'] * (data['weight']**alpha / wei_indeg_alpha[v])) - D4min) / (D4max  - D4min)
+            d4_in[v] += ((data['weight'] * (data['weight']**alpha / wei_outsum_alpha[u])) - D4min) / (D4max - D4min)
+            d4_out[u] += ((data['weight'] * (data['weight']**alpha / wei_insum_alpha[v])) - D4min) / (D4max  - D4min)
             
             d5_in[v] += ((1* (1 / outdeg[u]**alpha)) - D5min) / (D5max - D5min)
             d5_out[u] += ((1* (1 / indeg[v]**alpha)) - D5min) / (D5max - D5min)
