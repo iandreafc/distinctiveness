3eb77ab9bd499c29288db059aae7e8610022a3b3
Andrea Fronzetti Colladon <52541209+andreafcdev@users.noreply.github.com>
2020-02-13 15:16:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("Pypi.md", "r") as fh:
 
 setuptools.setup(
     name="distinctiveness",
-    version="0.0.9",
+    version="0.0.10",
     author="Andrea Fronzetti Colladon",
     author_email="a@andreafc.com",
     description="A Python package to calculate Distinctiveness Centrality",
